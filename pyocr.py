0bb4c0ff655d2bba3d9a4066d0ffd93cc86f3068
David Mosher <davidmosher@David-Moshers-iMac.local>
2010-10-14 21:57:46 -0600

    - file io - tests

--- pyocr.py
+++ pyocr.py
@@ -1,2 +1,20 @@
 """ Main script for PyOCRKata """
 
+import sys
+
+def main(argv):
+
+    if len(argv) > 2:
+        raise TypeError("pyocr only takes 1 argument, usage: pyocr.py [filename]")
+
+    filename = argv[1]
+
+    try:
+        file = open(filename)
+    except IOError:
+        print "Error opening file %s" % filename
+
+if __name__ == "__main__":
+
+    main(sys.argv)
+
--- tests/ocr_test.py
+++ tests/ocr_test.py
@@ -1,6 +1,18 @@
 from unittest import TestCase
+from pyocr import main
+
+class IOTest(TestCase):
+
+    def test_sanity(self):
+        self.assertTrue(True)
+
+    def test_main_raises_error_with_more_than_one_arg(self):
+        self.assertRaises(TypeError, main, *["foo.txt", "bar"])
+
+    def test_main_raises_error_when_file_is_not_found(self):
+        self.assertRaises(IOError, main, ["badfile.txt"])
 
 class OCRTest(TestCase):
 
     def test_sanity(self):
-        self.assertTrue(False)
+        self.assertTrue(True)
